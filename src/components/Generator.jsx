6d24c8f879bd494814d1bc50faa3079a3d942761
veronosmani <veron.osmani@gmail.com>
2024-06-18 14:20:29 +0200

    modified:   src/components/Generator.jsx 	modified:   src/components/Hero.jsx 	new file:   src/components/SectionWrapper.jsx 	modified:   src/index.css 	new file:   src/utils/swoldier.js

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import SectionWrapper from "./SectionWrapper";
+import { WORKOUTS } from "../utils/swoldier";
+
+function Header(props) {
+  const { index, title, description } = props;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">{index}</p>
+        <h4 className="text-lg sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
+    </div>
+  );
+}
 
 export default function Generator() {
   return (
-    <div>Generator</div>
-  )
+    <SectionWrapper
+      header={"Generate your workout"}
+      title={["It's", "Huge", "o'clock"]}
+    >
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+      {Object.keys(WORKOUTS).map((type, typeIndex) =>{
+        return (
+            <button className='bg-slate-950 border border-blue-400 py-3 rounded-lg' key={typeIndex}>
+                <p>{type}</p>
+            </button>
+        )
+      })}
+      </div>
+    </SectionWrapper>
+  );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function Hero() {
+export default function () {
   return (
-    <div>Hero</div>
-  )
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-4">
+        <p>IT'S TIME TO GET</p>
+        <h1 className="uppercase font-semibold text-6xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Swole<span className="text-blue-400">normous</span>
+        </h1>
+      </div>
+
+      <p className="text-sm md:text-base font-light">
+        I hereby acknowledgement that I may become{" "}
+        <span className="text-blue-400 font-medium">
+          unbelievably swolenormous
+        </span>{" "}
+        and accept all the risks of becoming the local{" "}
+        <span className="text-blue-400 font-medium">mass montrosity</span>,
+        afflicted with severe body dismporhia, unable to fit through doors.
+      </p>
+      <button className="px-8 py-4 rounded-medium border-[2px] bg-slate-950 border-blue-400 border-solid border blueShadow duration-200">
+        <p>Accept & Begin</p>
+      </button>
+    </div>
+  );
 }
