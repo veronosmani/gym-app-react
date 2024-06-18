ce85bcbe11944a14d47bb35b2a6c8005b0e781a8
veronosmani <veron.osmani@gmail.com>
2024-06-18 16:40:09 +0200

    modified:   src/App.jsx 	modified:   src/components/Button.jsx 	new file:   src/components/ExerciseCard.jsx 	modified:   src/components/Generator.jsx 	modified:   src/components/Hero.jsx 	modified:   src/components/Workout.jsx 	new file:   src/utils/functions.js

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
 
 export default function Button(props) {
-    const {text} = props
-  return (
-    <button className="px-8 py-4 rounded-medium border-[2px] bg-slate-950 border-blue-400 border-solid border blueShadow duration-200">
-    <p>{text}</p>
-  </button>
+    const { text, func } = props
+    return (
+        <button onClick={func} className='px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+            <p>{text}</p>
+        </button>
     )
 }
